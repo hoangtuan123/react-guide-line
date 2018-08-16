9f7001899ebd0c07979c95ce9b5a87944fc99e71
Tuan Nguyen <tuan.nguyen@exe.com.vn>
2018-08-16 17:39:17 +0700

    lists and state

--- src/App.js
+++ src/App.js
@@ -68,13 +68,10 @@ class App extends Component {
     if(this.state.showPerson){
       persons = (
         <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person click={this.switchName.bind(this, "newName!!!!!")} 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              changed={this.changeHanlder}
-              />
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            {this.state.persons.map(person =>{
+              return <Person name={person.name} 
+                              age={person.age} />
+            })}
         </div>
       )
     }
